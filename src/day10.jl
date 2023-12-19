df3eb206995545ee5f15e100c51d75e0b1c04610
heralden <regen@anche.no>
2023-12-19 22:46:55 +0100

    Day 10 part 2 - needed hint

--- src/day10.jl
+++ src/day10.jl
@@ -13,6 +13,15 @@ const legend = Dict(
   '.' => Set(),
 )
 
+const revlegend = Dict(value => key for (key, value) in legend)
+
+const opposelegend = Dict(
+  :n => :s,
+  :s => :n,
+  :e => :w,
+  :w => :e,
+)
+
 function adjacent(tiles, i, prevdir=nothing)
   (w, h) = size(tiles)
   (x, y) = Tuple(i)
@@ -23,10 +32,14 @@ function adjacent(tiles, i, prevdir=nothing)
     dirs = symdiff(Set([prevdir]), dirs)
   end
   adjacents = []
-  y > 1 && :n in dirs && push!(adjacents, (:s, i - CartesianIndex(0,1)))
-  x > 1 && :w in dirs && push!(adjacents, (:e, i - CartesianIndex(1,0)))
-  y < h && :s in dirs && push!(adjacents, (:n, i + CartesianIndex(0,1)))
-  x < w && :e in dirs && push!(adjacents, (:w, i + CartesianIndex(1,0)))
+  ni = i - CartesianIndex(0,1)
+  y > 1 && :n in dirs && :s in legend[tiles[ni]] && push!(adjacents, (:s, ni))
+  wi = i - CartesianIndex(1,0)
+  x > 1 && :w in dirs && :e in legend[tiles[wi]] && push!(adjacents, (:e, wi))
+  si = i + CartesianIndex(0,1)
+  y < h && :s in dirs && :n in legend[tiles[si]] && push!(adjacents, (:n, si))
+  ei = i + CartesianIndex(1,0)
+  x < w && :e in dirs && :w in legend[tiles[ei]] && push!(adjacents, (:w, ei))
   adjacents
 end
 
@@ -41,6 +54,9 @@ end
 function walk(tiles, start)
   max_steps = 1
   walkers = adjacent(tiles, start)
+  path = [start, map(last, walkers)...]
+  tiles[start] = revlegend[Set(map(x -> opposelegend[first(x)], walkers))]
+  # @show tiles[start]
   while !reduce(==, map(last, walkers))
     max_steps += 1
     next_walkers = []
@@ -49,15 +65,59 @@ function walk(tiles, start)
       push!(next_walkers, nextdirs...)
     end
     walkers = next_walkers
+    push!(path, map(last, next_walkers)...)
   end
-  max_steps
+  (max_steps, path)
 end
 
 function solve1(in)
   tiles = map(collect, filter(!isempty, split(in, "\n")))
   tiles = reshape(reduce(vcat, tiles), (length(tiles[1]),length(tiles)))
   start = find_start(tiles)
-  walk(tiles, start)
+  (max_steps, _) = walk(tiles, start)
+  max_steps
+end
+
+function calc_area(tiles, path)
+  within = false
+  count = 0
+  corner = nothing
+  for i in CartesianIndices(tiles)
+    (x, _) = Tuple(i)
+    if x == 1
+      # println()
+      within = false
+    end
+    if i in path
+      # print(tiles[i])
+      if tiles[i] == '|'
+        within = !within
+      elseif tiles[i] == 'F'
+        corner = 'F'
+      elseif tiles[i] == 'L'
+        corner = 'L'
+      elseif tiles[i] == '7' && corner == 'L'
+        within = !within
+      elseif tiles[i] == 'J' && corner == 'F'
+        within = !within
+      end
+    elseif within == true
+      # print('I')
+      # @show i
+      count += 1
+    else
+      # print('.')
+    end
+  end
+  count
+end
+
+function solve2(in)
+  tiles = map(collect, filter(!isempty, split(in, "\n")))
+  tiles = reshape(reduce(vcat, tiles), (length(tiles[1]),length(tiles)))
+  start = find_start(tiles)
+  (_, path) = walk(tiles, start)
+  calc_area(tiles, path)
 end
 
 end
--- test/day10.jl
+++ test/day10.jl
@@ -11,11 +11,33 @@ SJLL7
 |F--J
 LJ.LJ"
 
+ex2 = "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."
+
+ex3 = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"
+
 @testset "day10" begin
   @test solve1(ex1) == 8
   @test solve1(readinput("day10")) == 6714
-  # @test solve2(ex1) == 0
-  # @test solve2(readinput("day10")) == 0
+  @test solve2(ex2) == 4
+  @test solve2(ex3) == 10
+  @test solve2(readinput("day10")) == 429
 end
 
 end
